ec0c2fd4013929223caf8c2e50fb691862e664a2
Dwerkjem <derekrneilson@gmail.com>
2026-05-25 01:09:42 -0600

    added direnv hook

--- flake-outputs.nix
+++ flake-outputs.nix
@@ -108,6 +108,21 @@
                   XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS"
               fi
 
+              
+              _direnv_hook() {
+                trap -- "" SIGINT
+                eval "$("/nix/store/m9csyivb7b2d6bp7blb0sdc9mmqaypf3-direnv-2.37.1/bin/direnv" export zsh)"
+                trap - SIGINT
+              }
+              typeset -ag precmd_functions
+              if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
+                precmd_functions=(_direnv_hook $precmd_functions)
+              fi
+              typeset -ag chpwd_functions
+              if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
+                chpwd_functions=(_direnv_hook $chpwd_functions)
+              fi
+
               export EDITOR="$HOME/.nix-profile/bin/nvim"
               export VISUAL="$HOME/.nix-profile/bin/nvim"
               export SUDO_EDITOR="$HOME/.nix-profile/bin/nvim"
